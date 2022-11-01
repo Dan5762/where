df73629056c0252783811c7935f592f01595d131
Dan <Daniel@alberg-long.co.uk>
2022-11-01 14:54:59 +0000

    Working compass

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,156 @@
+import 'dart:math';
+
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:location/location.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Where',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(
+        body: MyHomePage(),
+      )
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key,}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Container(
+            height: 70,
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+            child: const CupertinoSearchTextField(
+              placeholder: 'Find Location',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            height: 60,
+            padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+            child: TextButton(
+              child: const Text(
+                'Copy Your Location',
+                textAlign: TextAlign.end,
+              ),
+              onPressed:() {},
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(20),
+              child: Compass()
+            )
+          ),
+        ]
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}class Compass extends StatefulWidget {
+
+	Compass({Key? key}) : super(key: key);
+
+	@override
+	_CompassState createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> with SingleTickerProviderStateMixin {
+
+	double _heading = 0;
+
+	String get _readout => _heading.toStringAsFixed(0) + '°';
+
+	@override
+	void initState() {
+		
+		super.initState();
+		FlutterCompass.events!.listen(_onData);
+	}
+	
+	void _onData(CompassEvent x) => setState(() { _heading = x.heading!; });
+
+	final TextStyle _style = const TextStyle(
+		color: Colors.red, 
+		fontSize: 32, 
+		fontWeight: FontWeight.w200,
+	);
+
+	@override
+	Widget build(BuildContext context) {
+
+		return CustomPaint(
+			foregroundPainter: CompassPainter(angle: _heading),
+			child: Center(child: Text(_readout, style: _style))
+		);
+	}
+}
+
+class CompassPainter extends CustomPainter {
+
+	CompassPainter({ required this.angle }) : super();
+
+  final double angle;
+	double get rotation => -2 * pi * (angle / 360);
+
+	Paint get _brush => Paint()
+		..style = PaintingStyle.stroke
+		..strokeWidth = 2.0;
+
+	@override
+	void paint(Canvas canvas, Size size) {
+
+		Paint circle = _brush
+			..color = Colors.indigo[400]!.withOpacity(0.6);
+
+		Paint needle = _brush
+			..color = Colors.red[400]!;
+		
+		double radius = min(size.width / 2, size.height / 2);
+		Offset center = Offset(size.width / 2, size.height / 2);
+		Offset centerL = Offset(size.width / 2 - 5, size.height / 2);
+		Offset centerR = Offset(size.width / 2 + 5, size.height / 2);
+    Offset point = Offset(center.dx, center.dy + radius);
+		Offset startL = Offset.lerp(centerL, point, 0.3)!;
+		Offset startR = Offset.lerp(centerR, point, 0.3)!;
+		Offset end = Offset.lerp(center, point, 0.9)!;
+		
+		canvas.translate(center.dx, center.dy);
+		canvas.rotate(rotation);
+		canvas.translate(-center.dx, -center.dy);
+		canvas.drawLine(startR, end, needle);
+		canvas.drawLine(startL, end, needle);
+		canvas.drawCircle(center, radius, circle);
+	}
+
+	@override
+	bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
