c2e8132285a35e67f43e0400544e776b8c4e731f
Dan <Daniel@alberg-long.co.uk>
2022-11-03 13:37:27 +0000

    implemmented computing bearing to given co-ordinate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,6 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:location/location.dart';
 import 'package:flutter_compass/flutter_compass.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const Scaffold(
+      home: const  Scaffold(
         body: MyHomePage(),
       )
     );
@@ -32,52 +32,156 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key,}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textController = TextEditingController();
+  bool targetLocSet = false;
+  LocationData deviceLoc =  LocationData.fromMap({
+    'latitude':  0.0,
+    'longitude': 0.0
+  });
+  late StreamSubscription<LocationData> locationSubscription;
+  LocationData targetLoc = LocationData.fromMap({
+    'latitude':  52.09751281363889,
+    'longitude': 0.2826702658818725
+  });
+  
+  _initLocationService() async {
+    Location location = Location();
+
+    if (!await location.serviceEnabled()) {
+      if (!await location.requestService()) {
+        return;
+      }
+    }
+
+    var permission = await location.hasPermission();
+    if (permission == PermissionStatus.denied) {
+      permission = await location.requestPermission();
+      if (permission != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    var loc = await location.getLocation();
+    setState(() {
+      deviceLoc = loc;
+      targetLocSet = true;
+    });
+
+    location.onLocationChanged.listen((loc) { 
+      print("${loc.latitude} ${loc.longitude}");
+      setState(() {
+        deviceLoc = loc;
+      });
+    });
+  }
+
+  Future<LocationData> getLocation() async {
+    var location = Location();
+    var loc = await location.getLocation();
+    return loc;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocationService();
+  }
+
+  double calculateAngle(LocationData loc1, LocationData loc2) {
+    double dLon = (loc2.longitude! - loc1.longitude!);
+
+    double y = sin(dLon) * cos(loc2.latitude!);
+    double x = cos(loc1.latitude!) * sin(loc2.latitude!) - sin(loc1.latitude!) * cos(loc2.latitude!) * cos(dLon);
+
+    double angle = atan2(y, x);
+
+    print("Angle: $angle");
+
+    return angle;
+  }
+
+  double calculateDistance(LocationData loc1, LocationData loc2) {
+    double phi1 = loc1.latitude! * pi/180;
+    double phi2 = loc2.latitude! * pi/180;
+
+    double dphi = (loc2.latitude! - loc1.latitude!) * pi/180;
+    double dlambda = (loc2.longitude! - loc1.longitude!) * pi/180;
+
+    double a = sin(dphi / 2) * sin(dphi / 2) + cos(phi1) * cos(phi2) * sin(dlambda / 2) * sin(dlambda / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+    double R = 6371000;
+    double distance = R * c;
+
+    print("Distance: $distance");
+
+    return distance;
+  }
 
   @override
   Widget build(BuildContext context) {
+    var angle = calculateAngle(deviceLoc, targetLoc);
+    var distance = calculateDistance(deviceLoc, targetLoc);
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Container(
             height: 70,
-            padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
-            child: const CupertinoSearchTextField(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: CupertinoSearchTextField(
+              controller: textController,
               placeholder: 'Find Location',
+              onSubmitted: (value) => {
+                setState(() => {
+                  targetLocSet = true,
+                  targetLoc = LocationData.fromMap({
+                    'latitude': double.parse(textController.text.split(',')[0]),
+                    'longitude': double.parse(textController.text.split(',')[1])
+                  }),
+                })
+              },
             ),
           ),
           Container(
             height: 60,
-            padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
-            child: TextButton(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 5),
+            child: CupertinoButton(
               child: const Text(
                 'Copy Your Location',
                 textAlign: TextAlign.end,
               ),
-              onPressed:() {},
+              onPressed:() async {
+                var loc = await getLocation();
+                print("${loc.latitude},${loc.longitude}");
+                await Clipboard.setData(ClipboardData(text: "${loc.latitude},${loc.longitude}"));
+              },
             ),
           ),
           Expanded(
             child: Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.all(20),
-              child: Compass()
+              child:  targetLocSet ? Compass(angle: angle, distance: distance) : Container()
             )
           ),
         ]
       ),
     );
   }
-}class Compass extends StatefulWidget {
+}
 
-	Compass({Key? key}) : super(key: key);
+class Compass extends StatefulWidget {
+  double angle;
+  double distance;
+  
+	Compass({Key? key, required this.angle, required this.distance}) : super(key: key);
 
 	@override
 	_CompassState createState() => _CompassState();
@@ -85,31 +189,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class _CompassState extends State<Compass> with SingleTickerProviderStateMixin {
 
-	double _heading = 0;
-
-	String get _readout => _heading.toStringAsFixed(0) + '°';
-
+	double deviceAngle = 0;
+	
 	@override
 	void initState() {
-		
 		super.initState();
 		FlutterCompass.events!.listen(_onData);
 	}
-	
-	void _onData(CompassEvent x) => setState(() { _heading = x.heading!; });
+
+	void _onData(CompassEvent x) {
+    setState(() {
+      deviceAngle = (-pi) - pi * (x.heading!) / 180;
+    });
+  }
 
 	final TextStyle _style = const TextStyle(
-		color: Colors.red, 
-		fontSize: 32, 
+		color: Colors.deepPurple, 
+		fontSize: 32,
 		fontWeight: FontWeight.w200,
 	);
 
 	@override
 	Widget build(BuildContext context) {
-
 		return CustomPaint(
-			foregroundPainter: CompassPainter(angle: _heading),
-			child: Center(child: Text(_readout, style: _style))
+			foregroundPainter: CompassPainter(angle: deviceAngle + widget.angle),
+			child: Center(child: Text(widget.distance.toStringAsPrecision(2) + 'm', style: _style))
 		);
 	}
 }
@@ -119,7 +223,7 @@ class CompassPainter extends CustomPainter {
 	CompassPainter({ required this.angle }) : super();
 
   final double angle;
-	double get rotation => -2 * pi * (angle / 360);
+	double get rotation => angle;
 
 	Paint get _brush => Paint()
 		..style = PaintingStyle.stroke
@@ -132,7 +236,7 @@ class CompassPainter extends CustomPainter {
 			..color = Colors.indigo[400]!.withOpacity(0.6);
 
 		Paint needle = _brush
-			..color = Colors.red[400]!;
+			..color = Colors.deepPurple[400]!;
 		
 		double radius = min(size.width / 2, size.height / 2);
 		Offset center = Offset(size.width / 2, size.height / 2);
